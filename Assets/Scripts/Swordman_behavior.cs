14b325f50f72270451776251438e380c3907ac34
DESKTOP-HNDQ1K7\yeongheon <yyh011119@gmail.com>
2021-08-16 19:00:12 +0900

    애니메이션 시간과 공격 판정 조정(미완성)

--- Assets/Scripts/Swordman_behavior.cs
+++ Assets/Scripts/Swordman_behavior.cs
@@ -8,6 +8,10 @@ public class Swordman_behavior : MonoBehaviour
     public Transform pos;
     public Vector2 boxSize;
 
+    private float attackTime;
+    private float dieTime;
+    private float runTime;
+
     public float health;
     private float currentHealth;
     public float moveSpeed;
@@ -31,11 +35,14 @@ public class Swordman_behavior : MonoBehaviour
         rigid = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
         Determine_Stats();
+        UpdateAnimClipTimes();
+        attackCooldown = 0.7f * attackTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(attackCooldown);
         DieCheck();
         if (isDie == false)
         {
@@ -58,8 +65,11 @@ public class Swordman_behavior : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision!");
-        unit_State = 2;
+        if (collision.tag == "Unit")
+        {
+            Debug.Log("Collision!");
+            unit_State = 2;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -70,17 +80,24 @@ public class Swordman_behavior : MonoBehaviour
     private void Attack()
     {
         anim.SetInteger("anime_State", 2);
-        Melee_Attack();
+        if (attackCooldown <= 0)
+        {
+            Melee_Attack();
+            attackCooldown = attackTime;
+        }
+        attackCooldown -= Time.deltaTime;
     }
 
     private void Run()
     {
+        attackCooldown = 0.5f * currentAttackSpeed;
         transform.localPosition += new Vector3(currentMoveSpeed * Time.deltaTime, 0, 0);
         anim.SetInteger("anime_State", 1);
     }
 
     private void Stay()
     {
+        attackCooldown = 0.5f * currentAttackSpeed;
         anim.SetInteger("anime_State", 0);
     }
 
@@ -111,45 +128,65 @@ public class Swordman_behavior : MonoBehaviour
 
     void DieCheck()
     {
-        if(currentHealth<=0)
+        if(currentHealth<=0&!isDie)
         {
-            if(!isDie)
-            {
-                Die();
-            }
+            Die();
         }
     }
 
     void Die()
     {
         isDie = true;
-
-        rigid.velocity = Vector2.zero;
-
         anim.SetBool("Die", true);
         Destroy(gameObject, 2);
-
     }
 
     void Melee_Attack()
     {
-        Debug.Log(currentHealth);
         Vector2 attackSpot = transform.Find("Detect").position;
-        Collider2D[] collider2Ds = Physics2D.OverlapPointAll(attackSpot);
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(attackSpot, detect_Collider.GetComponent<BoxCollider2D>().size, 0);
         foreach(Collider2D collider in collider2Ds)
         {
             if(collider.tag == "Unit")
             {
+                Debug.Log(currentHealth);
                 collider.GetComponent<Swordman_behavior>().TakeDamage(currentDamage);
+                return;
             }
         }
-
-
     }
 
     public void TakeDamage(float damage)
     {
+        
         currentHealth = currentHealth - damage;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.Find("Detect").position, detect_Collider.GetComponent<BoxCollider2D>().size);
+    }
+
+    public void UpdateAnimClipTimes()
+    {
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            switch (clip.name)
+            {
+                case "Melee_attack":
+                    attackTime = clip.length;
+                    break;
+                case "Melee_die":
+                    dieTime = clip.length;
+                    break;
+                case "Melee_run":
+                    runTime = clip.length;
+                    break;
+            }
+        }
+    }
+
 }
 
