92c40591e0fc07e61c0a69be07fe2f99b90c095f
LAPTOP-I4F2N6HA\duckh <duckhun02@gmail.com>
2021-08-18 21:45:01 +0900

    버튼 기능 통합

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    public int unitType;
+    public Vector3 unitPosition;
+
+    private CreateUnit create;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.create = GameObject.Find("Manager(temp)").GetComponent<CreateUnit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickButton()
+    {
+        create.unitcreate(unitType, unitPosition);
+    }
+}
--- Assets/Scripts/CreateUnit.cs
+++ Assets/Scripts/CreateUnit.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class CreateUnit : MonoBehaviour
 {
 
+    public GameObject[] unit;
+    public int[] cost;
+
+    private Vector3 mouse_position;
+    private PointControl pointcontrol;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        this.pointcontrol = GameObject.Find("Manager(temp)").GetComponent<PointControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(Resources.Load("Prefabs/Units/unit_1"), transform.Find("SpawnPoint").position, Quaternion.identity);
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Instantiate(Resources.Load("Prefabs/Units/unit_1"), transform.Find("SpawnPoint").position, Quaternion.identity);
+        //}
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Instantiate(Resources.Load("Prefabs/Enemies/Enemy_1"), transform.Find("SpawnPoint").position, Quaternion.identity);
+        //}
     }
 
+
+    public void unitcreate(int n, Vector3 position)
+    {
+        if (pointcontrol.point >= cost[n])
+        {
+            GameObject go = GameObject.Instantiate(this.unit[n]);
+            go.transform.position = position;
+            pointcontrol.point -= cost[n];
+        }
+    }
 }
+
 
