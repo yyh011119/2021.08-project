21aaf617714bedc7cdafe2259f398a74712c70e5
LAPTOP-I4F2N6HA\duckh <duckhun02@gmail.com>
2021-08-19 13:15:46 +0900

    버튼 통합 다시

---
+++ Assets/Scripts/PointControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PointControl : MonoBehaviour
+{
+    public int point;
+    public int maxPoint;
+    public GUIStyle style;
+
+    private Text pointText;
+    private float time;
+    private float checkTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointText = GameObject.Find("Point").GetComponent<Text>();
+        checkTime = 0.1f;
+        maxPoint = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > checkTime && point < maxPoint)
+        {
+            point++;
+            time = 0;
+        }
+
+
+        pointcounter();
+    }
+
+    void OnGUI()
+    {
+
+    }
+
+    void pointcounter()
+    {
+        pointText.text = point.ToString() + "/" + maxPoint.ToString();
+    }
+
+
+}
